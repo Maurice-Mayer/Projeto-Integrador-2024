13636508fa5583ad82e33402f8a0a75b0f2a4d22
Maurice Mayer <mau5920mau@gmail.com>
2024-06-15 17:04:01 -0300

    Projeto Integrador

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Exercício Principal
+
+// Começamos pedindo para o usuário entrar com as 4 notas necessárias
+// Perceba que colocamos uma exibição antes de cada entrada de dados apenas para na rodagem do programa fazer mais sentido uma "instrução" para o usuário.
+// Usamos int.parse para converter a entrada string do teclado em número inteiro.
+// E também o comando console.readline() para a entrada do dado.
+
+Console.WriteLine("DIGITE A PRIMEIRA NOTA: ");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("DIGITE A SEGUNDA NOTA: ");
+int n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("DIGITE A TERCEIRA NOTA: ");
+int n3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("DIGITE A QUARTA NOTA: ");
+int n4 = int.Parse(Console.ReadLine());
+
+// Agora presisamos calcular a média.
+// Somando as quatro notas dividindo pela quantidade de notas.
+
+double media = (n1 + n2+ n3+ n4) / 4;
+
+// Agora entramos com o "escolha caso". 
+// Se algo faça isso, Se outra coisa faça aquilo.
+// Repare que "if ele executa uma coisa e "else" ele executa outra coisa.
+// Perceba também a váriável "media" sendo ou não < (maior) que 7 rodando junto ao "else" executando "APROVADO".
+// Sendo < (menor) ele cai para a execução else executando a mensagem "REPROVADO".
+
+if(media > 7)
+{
+    Console.WriteLine("APROVADO. ");
+}
+else 
+{
+    Console.WriteLine("REPROVADO. ");
+}
+// Aqui chegamos ao fim do programa.
+
+
+
+
+
+
+
+
